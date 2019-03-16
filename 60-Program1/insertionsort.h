f3e1e0763e23439efc7e7029b0e48053bf9c2441
lanqing1 <48611043+lanqing1@users.noreply.github.com>
2019-03-15 21:36:15 -0700

    Delete insertionsort.h

--- 60-Program1/insertionsort.h
+++
@@ -1,9 +0,0 @@
-// Insertion Sort
-// 
-// Author: Rob Gysel
-// ECS60, UC Davis
-// Adapted from: Lysecky & Vahid "Data Structures Essentials", zyBooks
-
-#include <vector>
-
-void InsertionSort(std::vector<int>* numbers ,int* numOfComp, int* numOfMem);
